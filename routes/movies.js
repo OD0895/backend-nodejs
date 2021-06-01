ced92dd8699aa90654cbaf47d5f642983e1ae2e0
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-05-31 22:57:03 -0500

    :sparkles: [ADD] Middleware para el manejo de scopes de los usuarios

--- routes/movies.js
+++ routes/movies.js
@@ -9,6 +9,7 @@ const {
 } = require('../utils/schemas/movies');
 
 const validationHandler = require('../utils/middleware/validationHandler');
+const scopesValidationHandler = require('../utils/middleware/scopesValidationHandler');
 
 const cacheResponse = require('./../utils/cacheResponse');
 const {
@@ -25,24 +26,29 @@ function moviesApi(app) {
 
 	const moviesService = new MoviesService();
 
-	router.get('/', passport.authenticate('jwt', { session: false }), async function (req, res, next) {
-		cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
+	router.get('/', 
+		passport.authenticate('jwt', { session: false }),
+		scopesValidationHandler(['read:movies']),
+		async function (req, res, next) {
+			cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
 
-		const { tags } = req.query;
-		try {
-			const movies = await moviesService.getMovies({ tags });
-			res.status(200).json({
-				data: movies,
-				message: 'movies listed',
-			});
-		} catch (err) {
-			next(err);
+			const { tags } = req.query;
+			try {
+				const movies = await moviesService.getMovies({ tags });
+				res.status(200).json({
+					data: movies,
+					message: 'movies listed',
+				});
+			} catch (err) {
+				next(err);
+			}
 		}
-	});
+	);
 
 	router.get(
 		'/:movieId',
 		passport.authenticate('jwt', { session: false }),
+		scopesValidationHandler(['read:movies']),
 		validationHandler({ movieId: movieIdSchema }, 'params'),
 		async function (req, res, next) {
 			cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
@@ -65,6 +71,7 @@ function moviesApi(app) {
 	router.post(
 		'/',
 		passport.authenticate('jwt', { session: false }),
+		scopesValidationHandler(['create:movies']),
 		validationHandler(createMovieSchema),
 		async function (req, res, next) {
 			const { body: movie } = req;
@@ -84,6 +91,7 @@ function moviesApi(app) {
 	router.put(
 		'/:movieId',
 		passport.authenticate('jwt', { session: false }),
+		scopesValidationHandler(['update:movies']),
 		validationHandler({ movieId: movieIdSchema }, 'params'),
 		validationHandler(updateMovieSchema),
 		async function (req, res, next) {
@@ -109,6 +117,7 @@ function moviesApi(app) {
 	router.delete(
 		'/:movieId',
 		passport.authenticate('jwt', { session: false }),
+		scopesValidationHandler(['deleted:movies']),
 		validationHandler({ movieId: movieIdSchema }, 'params'),
 		async function (req, res, next) {
 			const { movieId } = req.params;
